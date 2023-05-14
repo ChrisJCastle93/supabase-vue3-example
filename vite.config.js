05fbeb3f9bcfa08ec659811069305a9f7350d4e3
Chris Castle <chrisjcastle93@gmail.com>
2023-05-14 21:46:50 +0200

    x

--- vite.config.js
+++ vite.config.js
@@ -21,69 +21,84 @@ export default defineConfig({
 						type: 'image/png',
 					},
 					{
-						src: 'img/icons/android-chrome-512x512.png',
-						sizes: '512x512',
+						src: 'img/icons/android-chrome-256x256.png',
+						sizes: '256x256',
 						type: 'image/png',
-					},
-					{
-						src: 'img/icons/android-chrome-maskable-192x192.png',
-						sizes: '192x192',
-						type: 'image/png',
-					},
-					{
-						src: 'img/icons/android-chrome-maskable-512x512.png',
-						sizes: '512x512',
-						type: 'image/png',
-					},
-					{
-						src: 'img/icons/apple-touch-icon-76x76.png',
-						sizes: '76x76',
-						type: 'image/png',
-					},
-					{
-						src: 'img/icons/apple-touch-icon-120x120.png',
-						sizes: '120x120',
-						type: 'image/png',
-					},
-					{
-						src: 'img/icons/apple-touch-icon-152x152.png',
-						sizes: '152x152',
-						type: 'image/png',
-					},
-					{
-						src: 'img/icons/apple-touch-icon-180x180.png',
-						sizes: '180x180',
-						type: 'image/png',
-					},
-					{
-						src: 'img/icons/apple-touch-icon.png',
-						type: 'image/png',
-					},
-					{
-						src: 'img/icons/favicon-16x16.png',
-						sizes: '16x16',
-						type: 'image/png',
-					},
-					{
-						src: 'img/icons/favicon-32x32.png',
-						sizes: '32x32',
-						type: 'image/png',
-					},
-					{
-						src: 'img/icons/msapplication-icon-144x144.png',
-						sizes: '144x144',
-						type: 'image/png',
-					},
-					{
-						src: 'img/icons/mstile-150x150.png',
-						sizes: '150x150',
-						type: 'image/png',
-					},
-					{
-						src: 'img/icons/safari-pinned-tab.svg',
-						type: 'image/svg',
 					},
 				],
+				// icons: [
+				// 	{
+				// 		src: 'img/icons/android-chrome-192x192.png',
+				// 		sizes: '192x192',
+				// 		type: 'image/png',
+				// 	},
+				// 	{
+				// 		src: 'img/icons/android-chrome-512x512.png',
+				// 		sizes: '512x512',
+				// 		type: 'image/png',
+				// 		purpose: 'any',
+				// 	},
+				// 	{
+				// 		src: 'img/icons/android-chrome-maskable-192x192.png',
+				// 		sizes: '192x192',
+				// 		type: 'image/png',
+				// 	},
+				// 	{
+				// 		src: 'img/icons/android-chrome-maskable-512x512.png',
+				// 		sizes: '512x512',
+				// 		type: 'image/png',
+				// 	},
+				// 	{
+				// 		src: 'img/icons/apple-touch-icon-76x76.png',
+				// 		sizes: '76x76',
+				// 		type: 'image/png',
+				// 	},
+				// 	{
+				// 		src: 'img/icons/apple-touch-icon-120x120.png',
+				// 		sizes: '120x120',
+				// 		type: 'image/png',
+				// 	},
+				// 	{
+				// 		src: 'img/icons/apple-touch-icon-152x152.png',
+				// 		sizes: '152x152',
+				// 		type: 'image/png',
+				// 	},
+				// 	{
+				// 		src: 'img/icons/apple-touch-icon-180x180.png',
+				// 		sizes: '180x180',
+				// 		type: 'image/png',
+				// 	},
+				// 	{
+				// 		src: 'img/icons/apple-touch-icon.png',
+				// 		sizes: '512x512',
+				// 		type: 'image/png',
+				// 		purpose: 'any',
+				// 	},
+				// 	{
+				// 		src: 'img/icons/favicon-16x16.png',
+				// 		sizes: '16x16',
+				// 		type: 'image/png',
+				// 	},
+				// 	{
+				// 		src: 'img/icons/favicon-32x32.png',
+				// 		sizes: '32x32',
+				// 		type: 'image/png',
+				// 	},
+				// 	{
+				// 		src: 'img/icons/msapplication-icon-144x144.png',
+				// 		sizes: '144x144',
+				// 		type: 'image/png',
+				// 	},
+				// 	{
+				// 		src: 'img/icons/mstile-150x150.png',
+				// 		sizes: '150x150',
+				// 		type: 'image/png',
+				// 	},
+				// 	{
+				// 		src: 'img/icons/safari-pinned-tab.svg',
+				// 		type: 'image/svg',
+				// 	},
+				// ],
 				short_name: 'Ale Blazers',
 				start_url: '/',
 				id: '/',
@@ -100,9 +115,6 @@ export default defineConfig({
 		watch: {
 			usePolling: true,
 		},
-	},
-	build: {
-		manifest: true,
 	},
 	resolve: {
 		alias: {
