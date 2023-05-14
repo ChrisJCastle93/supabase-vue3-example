476c879312bf3da3db86a50cd7f908e4d9ddf3ab
Chris Castle <chrisjcastle93@gmail.com>
2023-05-14 18:44:58 +0200

    x

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,27 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  server: {
-    watch: {
-      usePolling: true,
-    }
-  },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-    // alias: [
-    //   {
-    //     find: "@vue/runtime-core",
-    //     replacement: "@vue/runtime-core/dist/runtime-core.esm-bundler.js",
-    //   },
-    // ],
-  },
+	plugins: [vue(), vueJsx()],
+	server: {
+		watch: {
+			usePolling: true,
+		},
+	},
+	resolve: {
+		preserveSymlinks: true,
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+		// alias: [
+		//   {
+		//     find: "@vue/runtime-core",
+		//     replacement: "@vue/runtime-core/dist/runtime-core.esm-bundler.js",
+		//   },
+		// ],
+	},
 });
