c235287f1258b11218183dfbe0c733d02573a8b0
Chris Castle <chrisjcastle93@gmail.com>
2023-05-14 18:13:10 +0200

    x

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 import router from '@/router/router.js'
 import '@/assets/stylesheets/main.css'
 import App from './App.vue'
+import './registerServiceWorker'
 
 const app = createApp(App)
 app.use(createPinia())
