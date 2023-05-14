423550353a179997613bd3dc577ce508378ece5f
Chris Castle <chrisjcastle93@gmail.com>
2023-05-14 19:08:33 +0200

    x

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import { createPinia } from "pinia";
 import router from '@/router/router.js'
 import '@/assets/stylesheets/main.css'
 import App from './App.vue'
-import './registerServiceWorker'
 
 const app = createApp(App)
 app.use(createPinia())
--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,31 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
+// import vueJsx from '@vitejs/plugin-vue-jsx';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), vueJsx()],
+	plugins: [
+		vue(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true,
+			},
+		}),
+	],
 	server: {
 		watch: {
 			usePolling: true,
 		},
 	},
 	resolve: {
-		preserveSymlinks: true,
+		// preserveSymlinks: true,
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
+
 		// alias: [
 		//   {
 		//     find: "@vue/runtime-core",
